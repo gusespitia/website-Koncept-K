7bee9a36bad4073caa1632658b48ccff64b04d73
Gustavo Espitia <146354509+gusespitia@users.noreply.github.com>
2025-03-13 15:00:53 +0100

    over-ons

---
+++ app/overons/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../navbar'
+
+const page = () => {
+  return (
+    <div>
+         <Navbar />
+         pagina over ons</div>
+  )
+}
+
+export default page
