d349d44cf21bd1b01a077f19140d47f36283e280
dimitriwens <dimitriantonakos@gmail.com>
2025-03-13 15:01:33 +0100

    page setup

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../navbar'
+
+export const page = () => {
+  return (
+    <div>
+        <Navbar />
+        Contact</div>
+  )
+}
+export default page
---
+++ app/merken/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../navbar'
+
+const page = () => {
+  return (
+    <div>
+        <Navbar />
+    Onze merken</div>
+  )
+}
+
+export default page
